e07f59ff07fb181c4475a441ac44f29984838f98
DamianDeMie <damiandemie@gmail.com>
2020-02-19 11:59:33 +0100

    Started work on Party Stands

---
+++ js/script.js
@@ -0,0 +1,14 @@
+var progressBar = 3.33;
+var question = 0;
+var partyPosition = ['pro', 'contra', 'none'];
+var data = [];
+
+var partyOpinion
+var opinionPage
+var standTitle
+var standStatement
+
+function test() {
+    document.getElementById("startPage").style.display = "none";
+    document.getElementById("standPage").style.display = "block";
+}
