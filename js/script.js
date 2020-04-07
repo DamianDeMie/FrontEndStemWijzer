f3b2ecc468c7242a2e8576894a7ec51908c2379c
DamianDeMie <damiandemie@gmail.com>
2020-04-07 13:29:04 +0200

    Improved code

--- js/script.js
+++ js/script.js
@@ -1,27 +1,29 @@
 var progressBar = 100 / subjects.length;
 const progress = progressBar;
 var question = 0;
-var partyPosition = ['pro', 'contra', 'none'];
+var partyPosition = ["pro", "contra", "none"];
 var data = [];
 
-var standTitle = document.getElementById('standTitle');
-var standStatement = document.getElementById('standStatement');
+var standTitle = document.getElementById("standTitle");
+var standStatement = document.getElementById("standStatement");
 
-document.getElementById("progressBar").style.width = progressBar.toString() + "%"
+document.getElementById("progressBar").style.width =
+    progressBar.toString() + "%";
 
 standTitle.innerHTML = question + 1 + ". " + subjects[question].title;
 standStatement.innerHTML = subjects[question].statement;
 
-subjects.forEach(userInput => {
+subjects.forEach((userInput) => {
     userInput.myOpinion = "";
     userInput.important = false;
 });
 
+// Starts the Statement questionaire
 function start() {
     document.getElementById("startPage").style.display = "none";
     document.getElementById("standPage").style.display = "block";
 }
-
+// Goes back to previous statement, if you go all the way back it resets to the starting page.
 function backButton() {
     if (question == 0) {
         document.getElementById("startPage").style.display = "block";
@@ -33,28 +35,32 @@ function backButton() {
     }
 }
 
-function nextStatement(opinion) {
-    question = question + 1;
-    if (question == subjects.length - 1) {
-        progressBar = 100;
-    } else if (question == subjects.length) {
-        showResults();
-    } else {
+function nextStatement() {
+    if (question < subjects.length - 1) {
         progressBar = progressBar + progress;
+        question++;
+        continueStemWijzer();
+    } else {
+        progressBar = 100;
+        showResults();
     }
+}
 
+function getOpinion(opinion) {
     subjects[question].myOpinion = opinion;
-    continueStemWijzer();
+    nextStatement();
 }
 
 function continueStemWijzer() {
     standTitle.innerHTML = question + 1 + ". " + subjects[question].title;
     standStatement.innerHTML = subjects[question].statement;
 
-    document.getElementById("progressBar").style.width = progressBar.toString() + "%";
+    document.getElementById("progressBar").style.width =
+        progressBar.toString() + "%";
 }
 
 function showResults() {
     document.getElementById("standPage").style.display = "none";
     document.getElementById("resultsPage").style.display = "block";
+    console.log(subjects);
 }
